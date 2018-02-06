eece65e222dc09303b8bb3b2c9bfc900d676bfa5
Nick Sena <nicks9795@gmail.com>
2018-02-06 17:32:59 -0500

    Assignment1

---
+++ Assignment1.java
@@ -0,0 +1,20 @@
+/* Nick Sena
+CSC190 - Assignment 1
+
+1. nick9797 - username on git
+   TextWrangler - default text editor I will be using for git
+   nicks9795@gmail.com - User email
+   
+2.  (1) There are three criminala involved with Dr. Evil’s cyber missile project - Mini Me, Number Two & Dr. Evil himself.
+
+	(2) Because Mini me wrote 20 lines of code, he should receive 20 years in prison.
+	
+	(3) The difference between FINAL and THE_END is that they are targeting different locations. FINAL is targeting Argsfoh University, and THE_END is targeting The Mother Earth.
+
+	(4) CyberMissile.java was modified 6 times.
+	
+	(5) The GIT command to print out a tree structure of all git commands is: git log --all --graph --decorate --oneline --simplify-by-decoration. Mini Me merged all the branches.
+	
+3. In CyberMissile.java, fs.SetTarget should be changed from "Argsfoh University" to "The Moon", and wh.explode() to wh.selfDestruct().
+
+*/ 
